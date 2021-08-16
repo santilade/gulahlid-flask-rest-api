3519b1bba8e90a9f915db66cabc4dfe1c128e139
santilade <santiparlade97@gmail.com>
2021-08-16 18:35:41 +0200

    optimize code

--- app/__init__.py
+++ app/__init__.py
@@ -1,27 +1,31 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
+import secrets
+
 
 # Create database:
 db = SQLAlchemy()
 # Name database:
 DB_NAME = "db.sqlite"
+GENERATED_KEY = secrets.token_urlsafe(30)
 
 
 def create_app():
     # Initialize flask:
     app = Flask(__name__)
     # Encrypt cookies and session data (should be randomly generated):
-    app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
+    app.config['SECRET_KEY'] = GENERATED_KEY
+
     # Database location:
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     # Initialize database in this app:
     db.init_app(app)
 
-    # Import views:
+    # Import controller:
     from .controller import controller
-    # Register views:
+    # Register controller:
     app.register_blueprint(controller, url_prefix='/')  # Url_prefix creates subdomains
 
     # Create database tables if needed:
