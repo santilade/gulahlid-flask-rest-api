ec78c73c465216c5f735225f17bd10c0f7996411
santilade <santiparlade97@gmail.com>
2021-11-07 01:17:10 +0000

    calculate repeated shifts

--- app/generator.py
+++ app/generator.py
@@ -23,6 +23,7 @@ class Generator:
         self.serialized_unassigned_shifts = {}
         self.free_employees = {}  # {"week 1": {"Monday": [Employee, Employee, ...],"Tuesday": [], ...}, "week 2": ...}
         self.serialized_free_employees = {}
+        self.suggested_employees = {}
         self.switcher = {
             "daily rotations": {
                 "part-time": self.case1,
@@ -72,7 +73,7 @@ class Generator:
                 if employee_info.attendance[day]["afternoon"]:
                     self.add_free_employee(employee_info.employee_id, "", day, "afternoon")
 
-            self.assign_shifts(self.unassigned_shifts[day])
+            self.assign_shifts(self.unassigned_shifts[day], rotation)
 
     def case2(self):
         for rotation in range(1, self.calendar.total_rotations + 1):
@@ -99,7 +100,7 @@ class Generator:
                 if employee_info.attendance[day]["afternoon"]:
                     self.add_free_employee(employee_info.employee_id, "", day, "afternoon")
 
-            self.assign_shifts(self.unassigned_shifts[day])
+            self.assign_shifts(self.unassigned_shifts[day], rotation)
 
     def case3(self):
         for rotation in range(1, self.calendar.total_rotations + 1):
@@ -123,7 +124,7 @@ class Generator:
                     if employee_info.attendance[week][day]["afternoon"]:
                         self.add_free_employee(employee_info.employee_id, week, day, "afternoon")
 
-                self.assign_shifts(self.unassigned_shifts[week][day])
+                self.assign_shifts(self.unassigned_shifts[week][day], rotation)
 
     def case4(self):
         for rotation in range(1, self.calendar.total_rotations + 1):
@@ -156,18 +157,18 @@ class Generator:
                     if employee_info.attendance[week][day]["afternoon"]:
                         self.add_free_employee(employee_info.employee_id, week, day, "afternoon")
 
-                self.assign_shifts(self.unassigned_shifts[week][day])
+                self.assign_shifts(self.unassigned_shifts[week][day], rotation)
 
     def add_unassigned_shift(self, kid_id, week, day, time):
         kid = Kid.query.get(kid_id)
 
-        shift_priority = 5
-        if kid.closed_circle:
-            shift_priority = shift_priority - 2
-        if kid.difficulty == "challenging":
-            shift_priority = shift_priority - 1
-        if kid.employees_needed > 1:
-            shift_priority = shift_priority - 1
+        shift_priority = 1
+        if not kid.closed_circle:
+            shift_priority = shift_priority + 2
+        if kid.difficulty != "challenging":
+            shift_priority = shift_priority + 1
+        if kid.employees_needed == 1:
+            shift_priority = shift_priority + 1
 
         new_shift = Shift(self.calendar.id, week, day, time, shift_priority, 0, kid_id)
         db.session.add(new_shift)
@@ -181,33 +182,74 @@ class Generator:
             self.serialized_unassigned_shifts[week][day][time][shift_priority].append(shift_schema.dump(new_shift))
 
     def add_free_employee(self, employee_id, week, day, time):
-        employee = Employee.query.get(employee_id)
-
         if self.calendar.rotation_interval == 'daily rotations':
-            self.free_employees[day][time].append(employee)
-            self.serialized_free_employees[day][time].append(employee_schema.dump(employee))
+            self.free_employees[day][time].append(employee_id)
+            self.serialized_free_employees[day][time].append(employee_id)
         elif self.calendar.rotation_interval == 'weekly rotations':
-            self.free_employees[week][day][time].append(employee)
-            self.serialized_free_employees[week][day][time].append(employee_schema.dump(employee))
+            self.free_employees[week][day][time].append(employee_id)
+            self.serialized_free_employees[week][day][time].append(employee_id)
 
-    def assign_shifts(self, unassigned_shifts):
-        if "afternoon" in unassigned_shifts:
-            for shift in unassigned_shifts["afternoon"][1]:
+    def assign_shifts(self, unassigned_shifts, rotation):
+        for time in unassigned_shifts.keys():
+            self.suggested_employees[time] = {1: {}, 2: {}, 3: {}, 4: {}, 5: {}}
+            free_employees = []
+            for shift in unassigned_shifts[time][1]:
+                self.suggested_employees[time][1] = {shift.id: []}
                 # challenging / closed circle / 2 employees
-                kid = Kid.query.get(shift.kid_id)
-                possible_employees = kid.compatible_employees
-                print(possible_employees)
+                closed_circle_list = KidInfo.query.filter_by(calendar_id=self.calendar.id,
+                                                             kid_id=shift.kid_id)[0].closed_circle_list
+                if self.calendar.rotation_interval == "daily rotations":
+                    free_employees = self.free_employees[shift.day][shift.time]
+                elif self.calendar.rotation_interval == "weekly rotations":
+                    free_employees = self.free_employees[shift.week][shift.day][shift.time]
 
-            for shift in unassigned_shifts["afternoon"][2]:
+                print("kid: " + str(shift.kid_id) + " rotation: " + str(rotation))
+                print(closed_circle_list)
+                print(free_employees)
+
+                if rotation == 1:
+                    for pair in closed_circle_list:
+                        if all(x in free_employees for x in pair):
+                            print(pair)
+
+                            shift.employee_id = pair[0]
+                            new_shift = Shift(self.calendar.id, shift.week, shift.day, shift.time, shift.priority,
+                                              pair[1], shift.kid_id)
+                            db.session.add(new_shift)
+                            db.session.commit()
+
+                            free_employees.remove(pair[0])
+                            free_employees.remove(pair[1])
+                            break
+
+                elif rotation > 1:
+                    repeated = {}
+                    for i in range(1, rotation+1, 1):
+                        repeated["rotation " + str(i)] = []
+                        for pair in closed_circle_list:
+                            if self.calendar.rotation_interval == "daily rotations":
+                                repeated_shifts_employee_1 = Shift.query.filter_by(calendar_id=self.calendar.id,
+                                                                                   day=i,
+                                                                                   kid_id=shift.kid_id,
+                                                                                   employee_id=pair[0]
+                                                                                   )
+                                repeated_shifts_employee_2 = Shift.query.filter_by(calendar_id=self.calendar.id,
+                                                                                   day=i,
+                                                                                   kid_id=shift.kid_id,
+                                                                                   employee_id=pair[1]
+                                                                                   )
+
+
+            for shift in unassigned_shifts[time][2]:
                 # challenging / closed circle / 1 employees
                 return ""
-            for shift in unassigned_shifts["afternoon"][3]:
+            for shift in unassigned_shifts[time][3]:
                 # challenging / 2 employees
                 return ""
-            for shift in unassigned_shifts["afternoon"][4]:
+            for shift in unassigned_shifts[time][4]:
                 # average or easy-going / 2 employees
                 return ""
-            for shift in unassigned_shifts["afternoon"][5]:
+            for shift in unassigned_shifts[time][5]:
                 # average or easy-going / 1 employees
                 # list(itertools.product(unassigned_shifts["morning"][1], []))
                 return ""
